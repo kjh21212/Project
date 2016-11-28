8336557bdb1dee8305ac39f3d65317801194779e
kjh21212 <hello@helloui-MacBook-Pro.local>
2016-11-28 17:25:43 +0900

    콜재설정 완료
    김재훈

--- app/src/main/java/com/example/hello/project/Main.java
+++ app/src/main/java/com/example/hello/project/Main.java
@@ -1,10 +1,13 @@
 package com.example.hello.project;
-
+/*
+ 16.11.28 재커밋
+ */
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Main extends AppCompatActivity {
 
