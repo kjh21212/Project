8eb7523c7ccd813c61daff1267f1ae69f56f04a1
kjh21212 <hello@helloui-MacBook-Pro.local>
2016-11-28 17:21:41 +0900

    Revert "tournament1"
    This reverts commit 258190de22ed729072f93aaf4b2076f98949ddbb.

--- app/src/main/java/com/example/hello/project/Tournament.java
+++ app/src/main/java/com/example/hello/project/Tournament.java
@@ -1,26 +1,17 @@
 package com.example.hello.project;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-
-import java.util.Random;
 
 public class Tournament extends AppCompatActivity {
-    int[] img = {R.drawable.logo1, R.drawable.logo2, R.drawable.logo3, R.drawable.logo4, R.drawable.logo5, R.drawable.logo6, R.drawable.logo7, R.drawable.logo8
-            , R.drawable.logo9, R.drawable.logo10, R.drawable.logo11, R.drawable.logo12, R.drawable.logo13, R.drawable.logo14, R.drawable.logo15, R.drawable.logo16
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tournament);
-
-        TextView tv1 = (TextView)findViewById(R.id.tView1);
-        TextView tv2 = (TextView)findViewById(R.id.tView2);
         findViewById(R.id.backbutton5).setOnClickListener(
                 new Button.OnClickListener() {
                     public void onClick(View v) {
@@ -29,34 +20,6 @@ public class Tournament extends AppCompatActivity {
                     }
                 }
         );
-        findViewById(R.id.tView1).setOnClickListener(
-                new Button.OnClickListener() {
-                    public void onClick(View v) {
-                        Intent intent = new Intent(getApplicationContext(), Tournament.class);
-                        startActivity(intent);
-                    }
-                }
-        );
-        findViewById(R.id.tView2).setOnClickListener(
-                new Button.OnClickListener() {
-                    public void onClick(View v) {
-                        Intent intent = new Intent(getApplicationContext(), Tournament.class);
-                        startActivity(intent);
-                    }
-                }
-        );
-
-        Random ram1 = new Random();
-        Random ram2 = new Random();
-        //int[] num={};
-
-
-        int num1 = ram1.nextInt(img.length);
-        int num2 = ram2.nextInt(img.length);
-        //num[1] = ram.nextInt(img.length);
-
-        tv1.setBackgroundResource(img[num1]);
-        tv2.setBackgroundResource(img[num2]);
 
     }
 }
