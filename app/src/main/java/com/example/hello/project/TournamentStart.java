a0c15986d01bd4e3039a6e0b45d8c4d351f31ade
gilsub <naruwin@naver.com>
2016-11-30 03:56:51 +0900

    토너먼트디자인완성
    강현규

--- app/src/main/java/com/example/hello/project/TournamentStart.java
+++ app/src/main/java/com/example/hello/project/TournamentStart.java
@@ -9,8 +9,11 @@ import android.widget.Button;
     강현규 작업
 */
 public class TournamentStart extends AppCompatActivity {
-    public static int[] img = {R.drawable.logo1, R.drawable.logo2, R.drawable.logo3, R.drawable.logo4, R.drawable.logo5, R.drawable.logo6, R.drawable.logo7, R.drawable.logo8
-            , R.drawable.logo9, R.drawable.logo10, R.drawable.logo11, R.drawable.logo12, R.drawable.logo13, R.drawable.logo14, R.drawable.logo15, R.drawable.logo16
+    public static int[] img = {R.drawable.tour1, R.drawable.tour2, R.drawable.tour3, R.drawable.tour4, R.drawable.tour5, R.drawable.tour6, R.drawable.tour7, R.drawable.tour8, R.drawable.tour9
+            , R.drawable.tour10, R.drawable.tour11, R.drawable.tour12, R.drawable.tour13, R.drawable.tour14, R.drawable.tour15, R.drawable.tour16, R.drawable.tour17, R.drawable.tour18, R.drawable.tour19
+            , R.drawable.tour20, R.drawable.tour21, R.drawable.tour22, R.drawable.tour23, R.drawable.tour24, R.drawable.tour25, R.drawable.tour26, R.drawable.tour27, R.drawable.tour28, R.drawable.tour29
+            , R.drawable.tour30, R.drawable.tour31, R.drawable.tour32, R.drawable.tour33, R.drawable.tour34, R.drawable.tour35, R.drawable.tour36, R.drawable.tour37, R.drawable.tour38, R.drawable.tour39
+            , R.drawable.tour40, R.drawable.tour41, R.drawable.tour42, R.drawable.tour43, R.drawable.tour44, R.drawable.tour45, R.drawable.tour46, R.drawable.tour47, R.drawable.tour48
     };
     public static int[] simg=shuffle(img);
     public static int num[] = new int[8];
